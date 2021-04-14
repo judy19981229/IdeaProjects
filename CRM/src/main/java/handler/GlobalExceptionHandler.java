fa351e79d777e517a6410a5dc7318390998ae5c5
judy <1264284228@qq.com>
2021-04-14 18:33:14 +0800

    crm

--- CRM/src/main/java/handler/GlobalExceptionHandler.java
+++ CRM/src/main/java/handler/GlobalExceptionHandler.java
@@ -3,13 +3,16 @@ package handler;
 import exception.LoginException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(LoginException.class)
+    @ResponseBody
     public Map<String,Object> LoginException(Exception exception){
         Map<String,Object> map=new HashMap<>();
         map.put("success",false);
